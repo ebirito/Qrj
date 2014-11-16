c9fbd9782de70d3bdd98b4bf43a3ac44494b4792
Abraham Lapscher <ebirito@yahoo.com>
2014-11-16 13:31:06 -0500

    Put back background changes

--- PublicPages/Horoscope/View.aspx.cs
+++ PublicPages/Horoscope/View.aspx.cs
@@ -57,8 +57,7 @@ namespace QRJ.PublicPages.Horoscope
                 }
 
                 // Choose between the 12 background images
-                //int imageIndex = (clientDateTime.Day % 12) + 1;
-                int imageIndex = 1;
+                int imageIndex = (clientDateTime.Day % 12) + 1;
 
                 imgBackground.Src = "../../Content/themes/base/images/horoscopeBackgrounds/" + sign.ToString() + "/" + imageIndex + ".png";
                 txtHoroscope.InnerHtml = string.Format("{0}:<br /><br />{1}", clientDateTime.ToString("D"), horoscope.GetHoroscope(sign));
